f404b70fd57c23577d16a2c096c4e49053dc22f5
Jimintae <162279174+hackercatx86@users.noreply.github.com>
2024-03-26 17:35:59 +0900

    week5
    finish

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Welcome {
-
+	
 	public static void main(String[] args) {
+		
+		String[][] mbook = new String[NUM_BOOK][NUM_ITEM];
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -37,7 +39,7 @@ public class Welcome {
 						menuGuestInfo(name, phone);
 						break;
 					case 2:
-						menuCartItemList();
+						menuCartItemList(mbook);
 						break;
 					case 3:
 						menuCartClear();
@@ -90,7 +92,47 @@ public class Welcome {
 	 * Parameter: x
 	 * Return value: x  
 	 */
-	public static void menuCartItemList() {		System.out.println("2.장바구니 상품 목록 보기");	}
+	public static void menuCartItemList(String[][] book) {		System.out.println("2.장바구니 상품 목록 보기");
+		BookList(book);
+		for(int i = 0; i < NUM_BOOK; i++) {
+			for(int j = 0; j < NUM_ITEM; j++) {
+				System.out.print(book[i][j] + " | ");
+			}
+			System.out.println(" ");
+		}
+		
+		boolean quit = false;
+		
+		while(!quit) {
+			System.out.print("장바구니에 추가할 도서의 ID를 입력하세요: ");
+			
+			Scanner inPut = new Scanner(System.in);
+			String str = inPut.nextLine();
+			
+			boolean flag = false;
+			int numId = -1;
+			
+			for(int i = 0; i < NUM_BOOK; i++) {
+				if(str.equals(book[i][0])) {
+					numId = i;
+					flag = true;
+					break;
+				}
+			}
+			
+			if(flag) {
+				System.out.println("장바구니에 추가하시겠습니까? Y | N");
+				str = inPut.nextLine();
+				
+				if(str.toUpperCase().equals("Y")) {
+					System.out.println(book[numId][0] + " 도서가 장바구니에 추가되었습니다.");
+				}
+				quit = true;
+			}
+			else {
+				System.out.println("다시 입력해주세요");
+			}
+		}	}
 	/**
 	 * Explanation: number 3
 	 * Parameter: x
@@ -138,6 +180,35 @@ public class Welcome {
 	 */
 	public static void menuExit() {
 		System.out.println("8. 종료");
+	}
+	
+	static final int NUM_BOOK = 3;
+	static final int NUM_ITEM = 7;
+	
+	public static void BookList(String[][] book) {	
+		book[0][0] = "ISBN1234";
+		book[0][1] = "쉽게 배우는 JSP 웹 프로그래밍";
+		book[0][2] = "27000";
+		book[0][3] = "송미영";
+		book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
+		book[0][5] = "IT전문서";
+		book[0][6] = "2018/10/08";
+		
+		book[1][0] = "ISBN1235";
+		book[1][1] = "안드로이드 프로그래밍";
+		book[1][2] = "33000";
+		book[1][3] = "우재남";
+		book[1][4] = "실습 단계별 명쾌한 멘토링";
+		book[1][5] = "IT전문서";
+		book[1][6] = "2022/01/22";
+		
+		book[2][0] = "ISBN1236";
+		book[2][1] = "스크래치";
+		book[2][2] = "22000";
+		book[2][3] = "고광일";
+		book[2][4] = "컴퓨팅 사고력을 키우는 블록 코딩";
+		book[2][5] = "컴퓨터입문";
+		book[2][6] = "2019/06/10";
 	}
 	
 }
