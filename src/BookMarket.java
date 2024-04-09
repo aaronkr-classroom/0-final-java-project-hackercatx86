9b763e29c2b7df73ead6102be6d98f03830be89e
Jimintae <162279174+hackercatx86@users.noreply.github.com>
2024-04-09 15:05:49 +0900

    week6
    finish

--- src/BookMarket.java
+++
@@ -1,9 +0,0 @@
-
-public class BookMarket {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Shopping Mall!");
-		System.out.println("Welcome to Book Market!");
-	}
-
-}
